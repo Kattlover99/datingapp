dbcf5647e012fc2c3e5a14ce165ea0e5250ea73c
Kattlover99 <mikheil.chek@gmail.com>
2023-01-23 11:51:49 +0400

    add backend controllers

---
+++ backend/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using DatingApp.DTOs;
+using DatingApp.Entities;
+using DatingApp.Interface;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public AccountController(ITokenService tokenService, UserManager<AppUser> userManager, IMapper mapper)
+        {
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        [HttpPost("register")]  // api/account/register
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+            var user = _mapper.Map<AppUser>(registerDto);
+
+            user.UserName = registerDto.Username.ToLower();
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            var roleResult = await _userManager.AddToRoleAsync(user, "Member");
+            if (!roleResult.Succeeded) return BadRequest(roleResult.Errors);
+
+
+            return new UserDto
+            {
+                UserName = user.UserName,
+                Token = await _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.Users
+               .Include(p => p.Photos)
+               .SingleOrDefaultAsync(x =>
+            x.UserName == loginDto.Username.ToLower());   //FirstOrDefaultAsync()
+
+            if (user == null) return Unauthorized("Invalid Username");
+
+            var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (!result) return Unauthorized("Invalid password");
+
+            return new UserDto
+            {
+                UserName = user.UserName,
+                Token = await _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault()?.Url,
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
+
+            };
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
---
+++ backend/Controllers/UsersController.cs
@@ -0,0 +1,148 @@
+﻿using DatingApp.DTOs;
+using DatingApp.Entities;
+using DatingApp.Extensions;
+using DatingApp.Helpers;
+using DatingApp.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Controllers
+{
+
+    [Authorize]
+    public class UsersController : BaseApiController
+    {
+        private readonly IUnitOfWork _uow;
+
+        private readonly IPhotoService _photoService;
+
+        public UsersController(IUnitOfWork uow, IPhotoService photoService)
+        {
+            _uow = uow;
+            _photoService = photoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PagedList<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
+        {
+            var gender = await _uow.UserRepository.GetUserGender(User.GetUsername());
+            userParams.CurrentUsername = User.GetUsername();
+
+            if (string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = gender == "male" ? "female" : "male";
+            }
+
+            var users = await _uow.UserRepository.GetMembersAsync(userParams);
+
+            Response.AddPaginationHeader(new PaginationHeader(
+                users.CurrentPage,
+                users.PageSize,
+                users.TotalCount,
+                users.TotalPages
+                ));
+
+            return Ok(users);
+        }
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUserByUsername(string username)
+        {
+            return await _uow.UserRepository.GetUserByUsernameAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            string username = User.GetUsername();
+
+            if (_uow.UserRepository.UpdateUser(username, memberUpdateDto)) return NoContent();
+
+            return BadRequest("Failed to update the user");
+        }
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            var user = await _uow.UserRepository.GetAppUserByUsernameAsync(User.GetUsername());
+
+            if (user == null) return NotFound();
+
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if (result.Error != null) return BadRequest(result.Error.Message);
+
+            Photo photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId,
+                IsMain = false
+            };
+
+            if (user.Photos.Count == 0) photo.IsMain = true;
+
+            user.Photos.Add(photo);
+
+            if (await _uow.UserRepository.SaveAppUser(user))
+            {
+                return CreatedAtAction(nameof(GetUserByUsername),
+                    new { username = user.UserName }, new PhotoDto(photo));
+            }
+
+            return BadRequest("Problem adding photo");
+        }
+
+
+        [HttpPut("set-main-photo/{photoId}")]
+        public async Task<ActionResult> SetMainPhoto(int photoId)
+        {
+            var user = await _uow.UserRepository.GetAppUserByUsernameAsync(User.GetUsername());
+            if (user == null) return NotFound();
+
+            var photo = user.Photos.FirstOrDefault(photo => photo.Id == photoId);
+            if (photo == null) return NotFound();
+
+            if (photo.IsMain) return BadRequest("this is already your main photo");
+
+            var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
+            if (currentMain != null) currentMain.IsMain = false;
+            photo.IsMain = true;
+
+            if (await _uow.Complete()) return NoContent();
+
+            return BadRequest("Problem setting the main photo");
+        }
+
+
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+            var user = await _uow.UserRepository.GetAppUserByUsernameAsync(User.GetUsername());
+            if (user == null) return NotFound("User not found");
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+            if (photo == null) return NotFound();
+
+            if (photo.IsMain) return BadRequest("You cannot delete you main photo");
+
+            if (photo.PublicId != null)
+            {
+                var result = await _photoService.DeletePhotoAsync(photo.PublicId);
+                if (result.Error != null) return BadRequest(result.Error.Message);
+            }
+            user.Photos.Remove(photo);
+
+            if (await _uow.Complete()) return Ok();
+
+            return BadRequest("Problem deleting photo");
+
+        }
+
+        //[Authorize]   // apply from top class
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<AppUser>> GetUser(int id)
+        //{
+        //    return await _uow.UserRepository.GetUserByIdAsync(id);
+        //}
+    }
+}
